59ebb9248ed8f28d21748c315ff7449b81ace69e
JoeTerol <teroljoseeduardo@gmail.com>
2023-05-23 19:46:26 -0300

    new variables ON

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// VARIABLES
+
+let playNow = false;
+let PPM = 50; 
+let intervalo = undefined;
+const textPlaying = 'Empezar'
+const textStoped = 'Pausar';
+const titlePPM =  document.querySelector('#ppm');
+const buttonDecrease5PPM = document.querySelector('#buttton-decrease-5-ppm')
+const buttonDecrease1PPM = document.querySelector('#buttton-decrease-1-ppm')
+const buttonCrease5PPM = document.querySelector('#buttton-crease-5-ppm')
+const buttonCrease1PPM = document.querySelector('#buttton-crease-1-ppm')
+const buttonPlay = document.querySelector('#buttton-play')
+
+
